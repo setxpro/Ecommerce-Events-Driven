5d7e6a76b150b3570d12450c6d20b1769d85bfe4
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-10-06 09:31:24 -0300

    feat: create projects

---
+++ payments-historic-lambda/src/main/java/br/com/innoacct/payments_historic_lambda/PaymentsHistoricLambdaApplication.java
@@ -0,0 +1,13 @@
+package br.com.innoacct.payments_historic_lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsHistoricLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsHistoricLambdaApplication.class, args);
+	}
+
+}
---
+++ payments-historic-lambda/src/test/java/br/com/innoacct/payments_historic_lambda/PaymentsHistoricLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.innoacct.payments_historic_lambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsHistoricLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

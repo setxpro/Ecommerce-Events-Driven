6557827b30fc897cb8b8f62b71c906f4c26d9d60
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-10-10 17:24:10 -0300

    feat: AwsSnsService service

---
+++ payment-service/src/main/java/br/com/innoacct/payment_service/services/sns/AwsSnsService.java
@@ -0,0 +1,24 @@
+package br.com.innoacct.payment_service.services.sns;
+
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.Topic;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class AwsSnsService {
+    private final AmazonSNS snsClient;
+    private final Topic processedPaymentsTopic;
+
+    public AwsSnsService(AmazonSNS snsClient,
+                         Topic processedPaymentsTopic){
+        this.snsClient = snsClient;
+        this.processedPaymentsTopic = processedPaymentsTopic;
+    }
+
+    public void publish(String message){
+        snsClient.publish(processedPaymentsTopic.getTopicArn(), message);
+        System.out.println("Message published");
+    }
+}

82ecabbac36c01c5056230225a5409f694c2a628
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-10-10 17:22:23 -0300

    config: sqs and sns

---
+++ payment-service/src/main/java/br/com/innoacct/payment_service/config/SNSConfig.java
@@ -0,0 +1,31 @@
+package br.com.innoacct.payment_service.config;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.Topic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SNSConfig {
+
+    @Value("${aws.region}")
+    private String region;
+    @Value("${aws.sns.processed-orders-topic}")
+    private String processedOrdersTopicArn;
+
+    @Bean
+    public AmazonSNS amazonSNSBuilder(){
+        return AmazonSNSClientBuilder.standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .withRegion(region)
+                .build();
+    }
+
+    @Bean
+    public Topic snsProcessedOrdersTopicBuilder(){
+        return new Topic().withTopicArn(processedOrdersTopicArn);
+    }
+}
